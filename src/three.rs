15a53ddf1dda26a028e5106fc398c7a6673ff04f
Paolo <str237@gmail.com>
2022-12-05 06:38:00 +0100

    Three working

--- src/three.rs
+++ src/three.rs
@@ -1,16 +1,54 @@
-use std::cmp::{Eq, PartialEq};
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-fn read_data() -> () {
+fn read_data() -> Vec<String> {
   let filename = format!("./resources/3.txt");
   let file: File = File::open(&filename).expect(&format!("Cannot open file {}", &filename));
   let reader = BufReader::new(file);
   let line_iter = reader.lines();
-  line_iter
-    .map(|l| l.unwrap())
-    .map(|l| PlannedMove::from_line(&l))
-    .collect()
+  line_iter.map(|l| l.unwrap()).collect()
 }
 
-pub fn three() {}
+const A_UPPER: usize = 'A' as usize;
+const A_LOWER: usize = 'a' as usize;
+
+fn char_to_priority(c: char) -> usize {
+  let int = c as usize;
+  if int >= A_LOWER {
+    return int - A_LOWER + 1;
+  } else {
+    return int - A_UPPER + 27;
+  }
+}
+
+fn find_item(line: &String) -> char {
+  let n = line.len();
+  let (a, b) = line.split_at(n / 2);
+  let char_set: HashSet<char> = a.chars().collect();
+  for c in b.chars() {
+    if char_set.contains(&c) {
+      return c;
+    }
+  }
+  unreachable!()
+}
+
+pub fn three() {
+  let input = read_data();
+  let score: usize = input.iter().map(find_item).map(char_to_priority).sum();
+  println!("{score}");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::char_to_priority;
+
+  #[test]
+  fn priority() {
+    assert_eq!(1, char_to_priority('a'));
+    assert_eq!(26, char_to_priority('z'));
+    assert_eq!(27, char_to_priority('A'));
+    assert_eq!(52, char_to_priority('Z'));
+  }
+}
