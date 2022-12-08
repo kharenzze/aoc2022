e45cf81d34ff4399f47a684a42883974b00ab572
Paolo <str237@gmail.com>
2022-12-08 13:11:34 +0100

    Solved day 8 part 2

--- src/eight.rs
+++ src/eight.rs
@@ -1,4 +1,4 @@
-use crate::point::Point;
+use crate::point::{Direction, Point, DIRECTIONS};
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
@@ -25,7 +25,40 @@ fn read_data() -> Input {
     .collect()
 }
 
-fn solve(input: Input) -> usize {
+fn solve_v2(input: &Input) -> usize {
+  let w = input[0].len();
+  let h = input.len();
+  let dimensions = Point::new(h, w);
+  let mut best = 0_usize;
+  for i in 1..h {
+    for j in 1..w {
+      let pos = Point::new(i, j);
+      let value = input[i][j];
+      let mut view: Vec<usize> = vec![];
+      for dir in DIRECTIONS {
+        let mut count: usize = 0;
+        let mut current_pos = pos.clone();
+        while let Some(p) = current_pos.get_next(*dir, &dimensions) {
+          current_pos = p.clone();
+          count += 1;
+          let current_value = input[p.x][p.y];
+          if value <= current_value {
+            break;
+          }
+        }
+        view.push(count);
+      }
+
+      let score: usize = view.iter().product();
+      if score > best {
+        best = score;
+      }
+    }
+  }
+  best
+}
+
+fn solve(input: &Input) -> usize {
   let mut visible: HashSet<Point> = HashSet::new();
   let w = input[0].len();
   let h = input.len();
@@ -109,13 +142,13 @@ fn solve(input: Input) -> usize {
 
 pub fn eight() {
   let input = read_data();
-  let score = solve(input);
+  let score = solve_v2(&input);
   println!("{score}")
 }
 
 #[cfg(test)]
 mod tests {
-  use super::{char_to_usize, solve};
+  use super::{char_to_usize, solve, solve_v2};
 
   #[test]
   fn simple() {
@@ -124,6 +157,7 @@ mod tests {
       .iter()
       .map(|s| s.chars().map(char_to_usize).collect::<Vec<usize>>())
       .collect();
-    assert_eq!(solve(sample), 21);
+    assert_eq!(solve(&sample), 21);
+    assert_eq!(solve_v2(&sample), 8);
   }
 }
