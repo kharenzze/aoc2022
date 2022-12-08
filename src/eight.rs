06c7bfbb2a4123c678d960e81b548d9613a49f0a
Paolo <str237@gmail.com>
2022-12-08 10:38:39 +0100

    Structure and test

--- src/eight.rs
+++ src/eight.rs
@@ -1,24 +1,40 @@
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
-use std::iter::FromIterator;
 
-fn read_data() -> String {
+type Input = Vec<Vec<char>>;
+
+fn read_data() -> Input {
   let filename = format!("./resources/8.txt");
   let file: File = File::open(&filename).expect(&format!("Cannot open file {}", &filename));
   let reader = BufReader::new(file);
-  let mut line_iter = reader.lines();
-  line_iter.next().unwrap().unwrap()
+  let line_iter = reader.lines();
+  line_iter
+    .map(|l| l.unwrap().chars().collect::<Vec<char>>())
+    .collect()
+}
+
+fn solve(input: Input) -> usize {
+  unimplemented!()
 }
 
 pub fn eight() {
   let input = read_data();
-  unimplemented!()
+  let score = solve(input);
+  println!("{score}")
 }
 
 #[cfg(test)]
 mod tests {
+  use super::solve;
 
   #[test]
-  fn simple() {}
+  fn simple() {
+    let sample = vec!["30373", "25512", "65332", "33549", "35390"];
+    let sample = sample
+      .iter()
+      .map(|s| s.chars().collect::<Vec<char>>())
+      .collect();
+    assert_eq!(solve(sample), 21);
+  }
 }
