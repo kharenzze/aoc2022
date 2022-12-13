6ec543de43c8795dc965a94ea4107af32f072633
Paolo Raez <str237@gmail.com>
2022-12-13 11:11:09 +0100

    Monkey parsing

--- src/eleven.rs
+++ src/eleven.rs
@@ -1,3 +1,4 @@
+use std::default;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
@@ -14,7 +15,22 @@ fn read_data() -> Input {
 #[derive(Debug, Clone, Copy)]
 enum OpValue {
   Old,
-  number(isize),
+  Number(isize),
+}
+
+impl OpValue {
+  fn from_str(s: &str) -> Self {
+    match s {
+      "old" => OpValue::Old,
+      x => OpValue::Number(x.parse().unwrap()),
+    }
+  }
+}
+
+impl Default for OpValue {
+  fn default() -> Self {
+    Self::Old
+  }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -23,24 +39,125 @@ enum Operator {
   Mult,
 }
 
-#[derive(Debug, Clone, Copy)]
+impl Operator {
+  fn from_str(s: &str) -> Self {
+    match s {
+      "+" => Self::Sum,
+      "*" => Self::Mult,
+      _ => unreachable!(),
+    }
+  }
+}
+
+impl Default for Operator {
+  fn default() -> Self {
+    Self::Sum
+  }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
 struct Operation {
   a: OpValue,
   b: OpValue,
   operator: Operator,
 }
 
+#[derive(Debug, Clone, Copy, Default)]
+struct MonkeyTest {
+  test_value: isize,
+  target_true: usize,
+  target_false: usize,
+}
+
+impl MonkeyTest {
+  fn test(&self, v: isize) -> usize {
+    if v % self.test_value == 0 {
+      self.target_true
+    } else {
+      self.target_false
+    }
+  }
+}
+
+#[derive(Debug, Clone, Default)]
 struct Monkey {
   id: usize,
   items: Vec<isize>,
   operation: Operation,
-  test: isize,
+  test: MonkeyTest,
+}
+
+impl Monkey {
+  fn from_lines(lines: Vec<String>) -> Self {
+    let mut instance: Self = Default::default();
+    for (i, l) in lines.iter().enumerate() {
+      match i {
+        0 => {
+          let id = l
+            .strip_prefix("Monkey ")
+            .unwrap()
+            .strip_suffix(":")
+            .unwrap();
+          let id: usize = id.parse().unwrap();
+          instance.id = id;
+        }
+        1 => {
+          let items = l.trim().strip_prefix("Starting items: ").unwrap();
+          let items: Vec<isize> = items
+            .split(",")
+            .map(|item| item.trim().parse().unwrap())
+            .collect();
+          instance.items = items;
+        }
+        2 => {
+          let items = l.trim().strip_prefix("Operation: new = ").unwrap();
+          let items: Vec<&str> = items.split(" ").collect();
+          let op = Operation {
+            a: OpValue::from_str(items[0]),
+            b: OpValue::from_str(items[2]),
+            operator: Operator::from_str(items[1]),
+          };
+          instance.operation = op;
+        }
+        3 => {
+          let value: isize = l
+            .trim()
+            .strip_prefix("Test: divisible by ")
+            .unwrap()
+            .parse()
+            .unwrap();
+          instance.test.test_value = value;
+        }
+        4 => {
+          let value: usize = l
+            .trim()
+            .strip_prefix("If true: throw to monkey ")
+            .unwrap()
+            .parse()
+            .unwrap();
+          instance.test.target_true = value;
+        }
+        5 => {
+          let value: usize = l
+            .trim()
+            .strip_prefix("If false: throw to monkey ")
+            .unwrap()
+            .parse()
+            .unwrap();
+          instance.test.target_false = value;
+        }
+        _ => unreachable!(),
+      }
+    }
+
+    instance
+  }
 }
 
 fn solve(input: Input) -> usize {
   let filtered: Input = input.into_iter().filter(|s| s.is_empty()).collect();
   let grouped: Vec<Input> = filtered.chunks(6).map(|c| c.to_owned()).collect();
-
+  let monkeys: Vec<Monkey> = grouped.into_iter().map(Monkey::from_lines).collect();
   1
 }
 
