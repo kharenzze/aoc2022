cc2d87d4bee8ce36e592dbf8902f1b0948cc80ad
Paolo <str237@gmail.com>
2022-12-16 07:56:20 +0100

    Update template

--- src/template.rs
+++ src/template.rs
@@ -1,14 +1,14 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-type Input = usize;
+type Input = Vec<String>;
 
 fn read_data() -> Input {
   let filename = format!("./resources/7.txt");
   let file: File = File::open(&filename).expect(&format!("Cannot open file {}", &filename));
   let reader = BufReader::new(file);
   let line_iter = reader.lines();
-  unimplemented!()
+  line_iter.map(|l| l.unwrap()).collect()
 }
 
 fn solve(input: Input) -> usize {
