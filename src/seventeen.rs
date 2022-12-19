79e6a400d3776e4162ab6d7fa881ccceea69e846
Paolo <str237@gmail.com>
2022-12-19 22:06:05 +0100

    WIP parse input

--- src/seventeen.rs
+++ src/seventeen.rs
@@ -1,7 +1,58 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::ops::{Add, Sub};
 
 type Input = Vec<String>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Hash)]
+struct Point {
+  x: isize,
+  y: isize,
+}
+
+impl Add for Point {
+  type Output = Point;
+
+  fn add(self, other: Point) -> Point {
+    Point {
+      x: self.x + other.x,
+      y: self.y + other.y,
+    }
+  }
+}
+
+impl Sub for Point {
+  type Output = Point;
+
+  fn sub(self, other: Point) -> Point {
+    Point {
+      x: self.x - other.x,
+      y: self.y - other.y,
+    }
+  }
+}
+
+impl Point {
+  const fn new(x: isize, y: isize) -> Point {
+    Point { x, y }
+  }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum GasDir {
+  Left,
+  Right,
+}
+
+impl GasDir {
+  fn from_char(c: char) -> GasDir {
+    match c {
+      '<' => GasDir::Left,
+      '>' => GasDir::Right,
+      _ => unreachable!(),
+    }
+  }
+}
 
 fn read_data() -> Input {
   let filename = format!("./resources/17.txt");
@@ -12,6 +63,13 @@ fn read_data() -> Input {
 }
 
 fn solve(input: Input) -> usize {
+  let pattern: Vec<GasDir> = input
+    .first()
+    .unwrap()
+    .chars()
+    .map(GasDir::from_char)
+    .collect();
+  let mut pattern_iter = pattern.iter().cycle();
   unimplemented!()
 }
 
